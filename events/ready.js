11ac979c857a42008939be86c1d34582915f6468
OKTB929 <tunc.yaslan@gmail.com>
2021-01-04 15:32:21 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (bot, ready) => {
 	console.log(`${bot.user.tag} is now online!`);
-	bot.user.setActivity("OK!help for help | Coding!", {
+	bot.user.setActivity("AT!help for help | Coding!", {
 		 type: "STREAMING",
 		 url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 	   });
